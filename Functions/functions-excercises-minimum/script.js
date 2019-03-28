c15b5534b0da7d9a0edf4f22ef4550214a66fa3e
Sam Cherian <samnp87@gmail.com>
2019-03-28 21:03:23 +0530

    script file in function folder changed

--- Functions/functions-excercises-minimum/script.js
+++ Functions/functions-excercises-minimum/script.js
@@ -1,8 +1,9 @@
-function(a,b){
-  if(a<b){
+function min(a, b) {
+  if (a < b) {
     return console.log(a + " is min val");
-  }
-  else{
-    return console.log(b + "is min val")
+  } else {
+    return console.log(b + "is min val");
   }
 }
+
+min(4, 5);
