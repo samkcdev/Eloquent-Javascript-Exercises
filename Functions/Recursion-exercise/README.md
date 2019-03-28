327e49dcf73e680b951e284f2e3592f35a93e64e
Sam Cherian <samnp87@gmail.com>
2019-03-28 21:00:28 +0530

    Functions min exercise done

---
+++ Functions/Recursion-exercise/README.md
@@ -0,0 +1,7 @@
+# Eloquent-Javascript-Exercises
+
+These Exercises are from the chapter Functions from the Book
+
+Exercise One:
+
+Find minimum numbers
---
+++ Functions/Recursion-exercise/script.js
@@ -0,0 +1,10 @@
+function(a,b){
+  if(a<b){
+    return console.log(a + " is min val");
+  }
+  else{
+    return console.log(b + "is min val")
+  }
+}
+
+function(4,5);
