f74c43f4c062c9409345d7065942327e4d844706
Sam Cherian <samnp87@gmail.com>
2019-03-30 20:31:54 +0530

    Added note reagrding IIFE

---
+++ Functions/Notes/script-iife.js
@@ -0,0 +1,9 @@
+//The below code is called a IIFE
+//it gets immedialely after its written
+//dont have to invoke it separately
+var result = (function() {
+  let person = "Sam";
+  return person;
+})();
+
+console.log(result);
