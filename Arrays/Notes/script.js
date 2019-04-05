2f18e9b57d1e74c1ed1fabd6be84e2dbc197f4e7
Sam Cherian <samnp87@gmail.com>
2019-04-05 07:25:10 +0530

    push and unshift in arrays pop and unshift()

--- Arrays/Notes/script.js
+++ Arrays/Notes/script.js
@@ -2,27 +2,39 @@ let sampleArr = ["Sam", 2, "Biriyani"];
 
 // Adding a new value to array
 //push() adds value at the last of an array
-sampleArr.push("Animal Farm");
-//Adding a new value at the start of an array
-//unshift();
-
-console.log(sampleArr.unshift(666));
+//this doesnt create a new array
+//this just returns the new array length
+let pushArr = sampleArr.push("Animal Farm");
+console.log(pushArr);
+console.log(sampleArr);
+// ----------------------------------------------
+// Adding a new value at the start of an array
+// unshift();
+let unshiftArr = sampleArr.unshift(666);
+console.log(unshiftArr);
 console.log(sampleArr);
 
 //Removing a value from the back of an array
 //pop()
 
-console.log(sampleArr.pop());
+let popArr = sampleArr.pop();
+
+console.log(popArr);
 console.log(sampleArr);
 
 //Removing a value from the beggining of an array
-//unshift();
-console.log(sampleArr.shift());
+//shift();
+let shiftArr = sampleArr.shift();
+console.log(shiftArr);
 console.log(sampleArr);
+
+/*
+pop() and unshift() return the value that has been removed from the array
+*/
 
 //splice()
 
 //it can be used to both add and delete values from array
 
-sampleArr.splice(0, 1);
-console.log(sampleArr);
+// sampleArr.splice(0, 1);
+// console.log(sampleArr);
