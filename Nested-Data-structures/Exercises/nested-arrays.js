52b27ef27f92425b1182ee89eb78390873de3def
Sam Cherian <samnp87@gmail.com>
2019-05-11 18:19:36 +0530

    Tried to understand the nested array , kinda got a grasp of it. Come back to it.

--- Nested-Data-structures/Exercises/nested-arrays.js
+++ Nested-Data-structures/Exercises/nested-arrays.js
@@ -33,6 +33,13 @@ sumTotal();
 
 // Given the following array, write a function called countVowels, which returns the count of all of the vowels in each string regardless of case. To see if a value is an array, you can not use typeof since that will return 'object', so one way to do this is by using the Array.isArray function.
 
+// console.log(nestedArr[2][1][1][0]);
+//Another way of looping through the array values
+// for (const names of nestedArr) {
+//   console.log(names);
+// }
+
+var vowels = ["a", "e", "i", "o", "u"];
 var nestedArr = [
   "Elie",
   ["Matt", ["Tim"]],
@@ -41,12 +48,51 @@ var nestedArr = [
 ];
 
 function countVowels(arr) {
+  var count = 0;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      var innerVarVals = arr[i][j];
-      console.log(innerVarVals);
+    if (Array.isArray(arr[i])) {
+      console.log(i, arr[i]);
+      for (let j = 0; j < arr[i].length; j++) {
+        if (Array.isArray(arr[i][j])) {
+          console.log(i, j, arr[i][j]);
+          for (let n = 0; n < arr[i][j].length; n++) {
+            if (Array.isArray(arr[i][j][n])) {
+              console.log(i, j, n, arr[i][j][n]);
+              for (let k = 0; k < arr[i][j][n].lenght; k++) {
+                count = countChars(arr[i][j][n][k], count);
+                console.log("dafa", count);
+              }
+            } else {
+              count = countChars(arr[i][j][n], count);
+              console.log(i, j, n, arr[i][j][n]);
+            }
+          }
+        } else {
+          count = countChars(arr[i][j], count);
+          console.log(i, j, arr[i][j]);
+        }
+      }
+    } else {
+      count = countChars(arr[i], count);
+      console.log(i, arr[i]);
     }
   }
+  return count;
+}
+
+function checkVowels(char, count) {
+  if (vowels.includes(char.toLoweCase())) {
+    console.log(++count);
+    return ++count;
+  }
+  return count;
+}
+
+function countChars(str, count) {
+  for (let i = 0; i < str.lenght; i++) {
+    count = checkVowels(str[i], count);
+  }
+  return count;
 }
 
 countVowels(nestedArr);
