b224b9ccfaf1a5cde510cf735040b6461e04971f
Sam Cherian <samnp87@gmail.com>
2019-05-12 09:26:35 +0530

    Nested arays exercise done, it was fun

--- Nested-Data-structures/Exercises/nested-arrays.js
+++ Nested-Data-structures/Exercises/nested-arrays.js
@@ -97,4 +97,44 @@ var nestedArr = [
 
 // countVowels(nestedArr);
 
-displayArrs(nestedArr);
+// displayArrs(nestedArr);
+
+//Write a function called rotate which takes an array and a number, and moves each element however many spaces the number is to the right. For the value at the end of the array, rotate should move it back to the beginning.
+
+function rotate(arr, num) {
+  var amt = num % arr.length;
+  console.log(amt);
+  for (let a = 0; a < amt; a++) {
+    console.log(a, arr);
+    console.log(arr.unshift(arr.pop()));
+  }
+  console.log(arr);
+}
+
+rotate([1, 2, 3], 2);
+//couldnt solve this, had a vague idea. why divide num by arr length?
+//come back to this exercise.
+
+// Write a function called makeXOGrid which takes in two parameters, rows and columns, and returns an array of arrays with the number of values in each subarray equal to the columns parameter and the number of subarrays equal to the rows parameter. The values in the sub-arrays should switch between "X" and "O".
+
+function makeXOGrid(rows, columns) {
+  let parentArr = [];
+  let starWithX = true;
+  for (let a = 0; a < rows; a++) {
+    var rowArr = [];
+    for (let b = 0; b < columns; b++) {
+      if (starWithX) {
+        rowArr.push("X");
+      } else {
+        rowArr.push("O");
+      }
+      //switiching between true and false
+      // this is something to remember
+      starWithX = !starWithX;
+    }
+    parentArr.push(rowArr);
+  }
+  console.table(parentArr);
+}
+
+makeXOGrid(10, 4);
