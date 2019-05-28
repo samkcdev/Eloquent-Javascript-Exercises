0afeabc4f3aee53b8fbe8f003223d7621786d87b
Sam Cherian <samnp87@gmail.com>
2019-05-28 07:31:55 +0530

    randomGame exercise also done

--- Higher-Order-Functions/Exercises and Examples/exercises.js
+++ Higher-Order-Functions/Exercises and Examples/exercises.js
@@ -17,4 +17,23 @@ function countdown(num) {
   }
 }
 
-countdown(5);
+// countdown(5);
+
+//randomGame
+// Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and return the number of tries it took before we found a number greater than .75.
+
+function randomGame() {
+  var timerId = setInterval(randomNumberPicker, 1000);
+  var counter = 0;
+  function randomNumberPicker() {
+    var randomNumer = Math.random();
+    counter++;
+    console.log(randomNumer);
+    if (randomNumer > 0.75) {
+      clearInterval(timerId);
+    }
+    console.log(counter);
+  }
+}
+
+randomGame();
