5fb7df83fb5d323c3c07b723c13c8b0d3f060cce
Sam Cherian <samnp87@gmail.com>
2019-05-31 07:45:12 +0530

    completed exercise in  higher order functions

--- Higher-Order-Functions/Exercises and Examples/exercises.js
+++ Higher-Order-Functions/Exercises and Examples/exercises.js
@@ -125,3 +125,14 @@ findIndex([8, 11, 4, 27], function(val) {
 //specialMultiply
 
 // Write a function called specialMultiply which accepts two parameters. If the function is passed both parameters, it should return the product of the two. If the function is only passed one parameter - it should return a function which can later be passed another parameter to return the product. You will have to use closure and arguments to solve this.
+
+function specialMultiply(x, y) {
+  if (arguments.length === 1) {
+    return function(y) {
+      return x * y;
+    };
+  }
+  return x * y;
+}
+
+console.log(specialMultiply(5, 4));
