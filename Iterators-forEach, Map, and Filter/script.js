2694a732ef14ce42373edc5f9fa4c18f52006c07
Sam Cherian <samnp87@gmail.com>
2019-06-15 08:08:04 +0530

    Completed exercise for for Each

---
+++ Iterators-forEach, Map, and Filter/script.js
@@ -0,0 +1,22 @@
+function printFirstAndLast(arr) {
+  arr.forEach(element => {
+    console.log(element[0] + element[element.length - 1]);
+  });
+}
+printFirstAndLast(["awesome", "example", "of", "forEach"]);
+
+//Write a function called addKeyAndValue which accepts three parameters, an array (of objects), a key and a value. This function should return the array of objects after each key and value have been added to each object in the array.
+
+function addKeyAndValue(arr, key, value) {
+  arr.forEach(element => {
+    console.log((element[key] = value));
+  });
+  console.table(arr);
+  return arr;
+}
+
+addKeyAndValue(
+  [{ name: "Elie" }, { name: "Tim" }, { name: "Elie" }],
+  "isInstructor",
+  true
+);
