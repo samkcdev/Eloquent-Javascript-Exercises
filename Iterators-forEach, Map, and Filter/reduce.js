b8edde5ab11db48bf4e33d2394d695dda96c9c9d
Sam Cherian <samnp87@gmail.com>
2019-06-16 22:44:06 +0530

    Completed all the exercises in reduce. That felt soo good.

--- Iterators-forEach, Map, and Filter/reduce.js
+++ Iterators-forEach, Map, and Filter/reduce.js
@@ -3,7 +3,6 @@
 function extractKey(arrObj, key) {
   arrObj.reduce(function(acc, next) {
     acc.push(next[key]);
-    console.log(acc);
     return acc;
   }, []);
 }
@@ -19,17 +18,40 @@ extractKey(
 
 //Write a function called filterLetters which accepts an array of letters and returns the number of occurrences of a specific letter. This function should be case insensitive
 
-// function filterLetters(arr, letter) {
-//   arr.reduce(function(acc, next) {
-//     if (next.toLowerCase() !== letter.toLowerCase()) {
-//       return acc;
-//     } else {
-//       console.log(acc + 1);
-//       return acc + 1;
-//     }
-//   }, 0);
-// }
+function filterLetters(arr, letter) {
+  arr.reduce(function(acc, next) {
+    if (next.toLowerCase() !== letter.toLowerCase()) {
+      return acc;
+    } else {
+      return acc + 1;
+    }
+  }, 0);
+}
 
-// filterLetters(["a", "a", "b", "c", "A"], "a"); // 3
-// filterLetters(["a", "a", "b", "c", "A"], "z"); // 0
-// filterLetters(["a", "a", "b", "c", "A"], "B"); // 1
+filterLetters(["a", "a", "b", "c", "A"], "a"); // 3
+filterLetters(["a", "a", "b", "c", "A"], "z"); // 0
+filterLetters(["a", "a", "b", "c", "A"], "B"); // 1
+
+// var arr = [1, 2, 3, 4];
+// arr.reduce(function(acc, next) {
+//   if (next >= 2) {
+//     acc["key" + next] = next;
+//   }
+//   return acc;
+// }, {});
+
+//Write a function called addKeyAndValue which accepts three parameters, an array (of objects), a key and a value. This function should return the array of objects after each key and value has been added. You can do this a few ways, either by reducing starting with an empty array and making copies of the object or by starting with the actual array!
+
+function addKeyAndValue(arr, key, val) {
+  arr.reduce(function(acc, next) {
+    next[key] = val;
+    acc.push(next);
+    return acc;
+  }, []);
+}
+
+addKeyAndValue(
+  [{ name: "Elie" }, { name: "Tim" }, { name: "Elie" }],
+  "isInstructor",
+  true
+);
