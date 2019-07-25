b39f3db0ecbf6593559cd2cd086d7a23ad9a859f
Sam Cherian <samnp87@gmail.com>
2019-07-25 08:06:22 +0530

    Started with  new jasmine exercise

--- Testing-with-Jasmine/test-jasmine.js
+++ Testing-with-Jasmine/test-jasmine.js
@@ -20,19 +20,35 @@
 
 //Write a function called replaceWith that takes in a string, a character to replace and a character to replace it with and returns the string with the replacements. Write tests to make sure this is case sensitive
 
-describe("replacewith", function() {
-  it("is case sensitive", function() {
-    expect(replaceWith("Awesome", "e", "b")).toBe("awbsomb");
+// describe("replacewith", function() {
+//   it("is case sensitive", function() {
+//     expect(replaceWith("for Me", "e", "b")).toBe("for Mb");
+//   });
+// });
+
+// function replaceWith(string, displacedChar, newChar) {
+//   var splitString = string.split("");
+//   for (let i = 0; i < splitString.length; i++) {
+//     if (splitString[i] === displacedChar) {
+//       splitString[i] = newChar;
+//     }
+//   }
+//   var newString = splitString.join("");
+//   return newString;
+// }
+
+//Write a function called expand which takes an array and a number and returns a copy of the array with as many numbers as specified
+
+describe("expand", function() {
+  it("returns any array", function() {
+    expect(expand([1, 2, 3], 3)).toBe(jasmine.any(Array));
   });
 });
 
-function replaceWith(string, displacedChar, newChar) {
-  var splitString = string.split("");
-  for (let i = 0; i < splitString.length; i++) {
-    if (splitString[i] === displacedChar) {
-      splitString[i] = newChar;
-    }
+function expand(arr, repeatArr) {
+  var newArr = [];
+  for (let i = 0; i < repeatArr; i++) {
+    newArr.push(arr.slice(0, arr.length));
   }
-  var newString = splitString.join("");
-  return newString;
+  console.log(newArr);
 }
