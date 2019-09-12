a06d47d02abb19480d9b9cce471e413fb7ec5e53
Sam Cherian <samnp87@gmail.com>
2019-09-12 21:35:21 +0530

    OOP exercises

--- OOP/Exercises/script.js
+++ OOP/Exercises/script.js
@@ -10,3 +10,21 @@ let rusty = new Dog("Rusty", 3);
 let fido = new Dog("Fido", 5);
 
 rusty.bark();
+
+//using the call function to borrow properties from another constructor
+
+function Car(model, type, year) {
+  this.model = model;
+  this.type = type;
+  this.year = year;
+  this.numberOfwheels = 4;
+}
+
+function MotorCycle(model, type, year, make) {
+  Car.call(this, model, type, year);
+  this.numberOfwheels = 2;
+  this.make = make;
+}
+
+ducatti = new MotorCycle("Ducatti", "Superbike", 2000, "Ducattis");
+console.table(ducatti);
